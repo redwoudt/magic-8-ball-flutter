f3df4e6e5be23413195a4741d7836187c02682be
Ferdi Redelinghuys <ferdi.redelinghuys@vulamobile.com>
2020-10-09 13:20:19 +0200

    created basic app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask me Anything'),
+      ),
+      body: Container(),
+    );
+  }
+}
